e3d0aa2f3e42f9fb78b9057124ba1f378c894853
Martin Unzner <martin.u@posteo.de>
2020-12-25 22:56:25 +0100

    force recreation of containers for reproducible builds

--- ci/run-ci.sh
+++ ci/run-ci.sh
@@ -12,4 +12,4 @@ ssh-keygen -b 2048 -t rsa -f testmachine -q -N ""
 echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" > .env
 echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" > .env
 docker-compose build --pull
-docker-compose up --abort-on-container-exit
+docker-compose up --force-recreate --abort-on-container-exit
