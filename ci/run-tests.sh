c9f370004519bfd19a84191e69285bdf84a795a2
Martin Unzner <martin.u@posteo.de>
2020-12-28 19:02:11 +0100

    generalize role name

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -9,7 +9,7 @@ run_ansible()
     ansible-playbook \
         -e aws_access_key="$AWS_ACCESS_KEY_ID" \
         -e aws_secret_key="$AWS_SECRET_ACCESS_KEY" \
-        -v /etc/ansible/roles/ansible-role-ghost-restore/tests/test.yml | tee ansible.log
+        -v /etc/ansible/roles/role-under-test/tests/test.yml | tee ansible.log
     if grep -q 'failed=[1-9]' ansible.log; then
         echo 'Ansible failed'
         exit 1
@@ -18,13 +18,13 @@ run_ansible()
 
 . venv/bin/activate
 echo "Install requirements"
-pip install -r /etc/ansible/roles/ansible-role-ghost-restore/requirements.txt
+pip install -r /etc/ansible/roles/role-under-test/requirements.txt
 echo "Install roles"
-ansible-galaxy install -r /etc/ansible/roles/ansible-role-ghost-restore/roles/requirements.yml
+ansible-galaxy install -r /etc/ansible/roles/role-under-test/roles/requirements.yml
 echo "Check syntax"
-ansible-playbook -v /etc/ansible/roles/ansible-role-ghost-restore/tests/test.yml --syntax-check
+ansible-playbook -v /etc/ansible/roles/role-under-test/tests/test.yml --syntax-check
 echo "Run linter"
-ansible-lint /etc/ansible/roles/ansible-role-ghost-restore
+ansible-lint /etc/ansible/roles/role-under-test
 echo "Run ansible"
 run_ansible
 echo "Run idempotence test"
